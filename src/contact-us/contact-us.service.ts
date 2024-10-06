4caa9a446af621255a35ce7998cea6fb65dafc3e
Shtefanika Vadym <vadym.shtefanika.assist@natterbox.com>
2024-10-06 16:27:10 +0300

    Remove package lock

--- src/contact-us/contact-us.service.ts
+++ src/contact-us/contact-us.service.ts
@@ -12,6 +12,7 @@ export class ContactUsService {
 
   async create(dto: CreateContactDto): Promise<MessageResponse> {
     await this.contactUsRepository.create(dto);
+    console.log();
     return {
       message: "Thanks! We will contact you shortly",
     };
